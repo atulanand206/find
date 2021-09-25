869367bd86060e39f87ab70f25f900aad880e1b5
Atul <atulanand206@gmail.com>
2021-09-25 23:48:48 +0530

    endpoints for q/a

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -2,18 +2,23 @@ package routes
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/atulanand206/go-mongo"
 	net "github.com/atulanand206/go-network"
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	mg "go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
+	PlayersCount = 2
+
 	Err_RequestNotDecoded      = "Request can't be decoded."
 	Err_MatchNotPresent        = "Match with the given info does not exist."
 	Err_MatchNotDecoded        = "Match information can't be decoded."
@@ -23,24 +28,69 @@ const (
 	Err_PlayerNotCreated       = "Player can't be created."
 	Err_PlayerNotDecoded       = "Player information can't be decoded."
 	Err_PlayerNotPresent       = "Player with the given info does not exist."
+	Err_QuizmasterCantPlay     = "Quizmaster can't join the match as a player."
+	Err_WaitingForPlayers      = "Waiting for more players to join."
+	Err_QuestionsRequestFailed = "Questions count request failed."
+	Err_QuestionNotCreated     = "Question can't be created."
+	Err_AnswerNotCreated       = "Answer can't be created."
+	Err_AnswerNotPresent       = "Answer does not exist."
 )
 
 type (
 	Player struct {
-		Id    uuid.UUID `json:"id" bson:"_id"`
-		Name  string    `json:"name" bson:"name"`
-		Email string    `json:"email" bson:"email"`
+		Id    string `json:"id" bson:"_id"`
+		Name  string `json:"name" bson:"name"`
+		Email string `json:"email" bson:"email"`
 	}
 
 	Game struct {
-		Id         uuid.UUID `json:"id" bson:"_id"`
-		Players    []Player  `json:"players" bson:"players"`
-		QuizMaster Player    `json:"quizmaster" bson:"quizmaster"`
+		Id         string   `json:"id" bson:"_id"`
+		Players    []Player `json:"players" bson:"players"`
+		QuizMaster Player   `json:"quizmaster" bson:"quizmaster"`
 	}
 
 	EnterGameRequest struct {
 		Person  Player `json:"person"`
 		MatchId string `json:"match_id"`
+	}
+
+	StartGameRequest struct {
+		MatchId string `json:"match_id"`
+	}
+
+	StartGameResponse struct {
+		Match  Game     `json:"game"`
+		Prompt Question `json:"prompt"`
+	}
+
+	Question struct {
+		Id         string   `json:"id" bson:"_id"`
+		Statements []string `json:"statements" bson:"statements"`
+	}
+
+	Answer struct {
+		Id         string `json:"id" bson:"_id"`
+		QuestionId string `json:"question_id" bson:"question_id"`
+		Answer     string `json:"answer" bson:"answer"`
+	}
+
+	AddQuestionRequest struct {
+		Statements []string `json:"statements"`
+		Answer     string   `json:"answer"`
+	}
+
+	AddQuestionResponse struct {
+		QuestionId string `json:"question_id"`
+		AnswerId   string `json:"answer_id"`
+	}
+
+	FindAnswerRequest struct {
+		QuestionId string `json:"question_id"`
+	}
+
+	FindAnswerResponse struct {
+		QuestionId string `json:"question_id"`
+		Answer     string `json:"answer"`
 	}
 )
 
@@ -51,6 +101,8 @@ var (
 	MatchCollection string
 	// Instance variable to store the DB Question Collection name.
 	QuestionCollection string
+	// Instance variable to store the DB Answer Collection name.
+	AnswerCollection string
 	// Instance variable to store the DB Match Collection name.
 	PlayerCollection string
 )
@@ -68,6 +120,7 @@ func Routes() *http.ServeMux {
 	Database = os.Getenv("GAME_DATABASE")
 	MatchCollection = os.Getenv("MATCH_COLLECTION")
 	QuestionCollection = os.Getenv("QUESTION_COLLECTION")
+	AnswerCollection = os.Getenv("ANSWER_COLLECTION")
 	PlayerCollection = os.Getenv("PLAYER_COLLECTION")
 
 	// Interceptor chain for attaching to the requests.
@@ -84,8 +137,9 @@ func Routes() *http.ServeMux {
 	router := http.NewServeMux()
 	router.HandleFunc("/enter", postChain.Handler(HandlerEnterGame))
 	router.HandleFunc("/start", postChain.Handler(HandlerStart))
+	router.HandleFunc("/question/add", postChain.Handler(HandlerAddQuestion))
 	router.HandleFunc("/question/next", getChain.Handler(HandlerNextQuestion))
-	router.HandleFunc("/question/verify", getChain.Handler(HandlerVerifyAnswer))
+	router.HandleFunc("/question/verify", getChain.Handler(HandlerFindAnswer))
 	return router
 }
 
@@ -116,7 +170,7 @@ func CreateOrFindPlayer(w http.ResponseWriter, playerRequest Player) (player Pla
 		return
 	}
 	player = playerRequest
-	player.Id, _ = uuid.New()
+	player.Id = uuid.New().String()
 	requestDto, _ := mongo.Document(player)
 	if err != nil {
 		_, err = mongo.Write(Database, PlayerCollection, *requestDto)
@@ -155,29 +209,32 @@ func CreateOrUpdateMatch(w http.ResponseWriter, matchId string, person Player) (
 		}
 	}
 	if err != nil {
-		match.Id, _ = uuid.New()
+		match.Id = uuid.New().String()
+		match.Players = make([]Player, 0)
 		match.QuizMaster = person
 		requestDto, _ := mongo.Document(match)
 		insertMatchResult, er := mongo.Write(Database, MatchCollection, *requestDto)
+		err = nil
 		if er != nil {
 			http.Error(w, Err_MatchNotCreated, http.StatusInternalServerError)
 			return
 		}
 		matchId = fmt.Sprint(insertMatchResult.InsertedID)
 	} else {
+		if match.QuizMaster.Id == person.Id {
+			err = errors.New(Err_QuizmasterCantPlay)
+			http.Error(w, Err_QuizmasterCantPlay, http.StatusInternalServerError)
+			return
+		}
 		match.Players = append(match.Players, person)
 		requestDto, _ := mongo.Document(match)
-		_, err = mongo.Update(Database, MatchCollection, bson.M{"_id": matchId}, *requestDto)
+		_, err = mongo.Update(Database, MatchCollection, bson.M{"_id": matchId}, bson.D{primitive.E{Key: "$set", Value: *requestDto}})
 		if err != nil {
 			http.Error(w, Err_MatchNotUpdated, http.StatusInternalServerError)
 			return
 		}
 	}
 	dto = mongo.FindOne(Database, MatchCollection, bson.M{"_id": matchId})
-	if err != nil {
-		http.Error(w, Err_MatchNotDecoded, http.StatusInternalServerError)
-		return
-	}
 	match, err = DecodeMatch(dto)
 	return
 }
@@ -192,11 +249,111 @@ func DecodeMatch(document *mg.SingleResult) (v Game, err error) {
 }
 
 func HandlerStart(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var requestBody StartGameRequest
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		http.Error(w, Err_RequestNotDecoded, http.StatusInternalServerError)
+		return
+	}
+	dto := mongo.FindOne(Database, MatchCollection, bson.M{"_id": requestBody.MatchId})
+	err = dto.Err()
+	if err != nil {
+		http.Error(w, Err_MatchNotPresent, http.StatusInternalServerError)
+		return
+	}
+	match, err := DecodeMatch(dto)
+	if len(match.Players) != PlayersCount {
+		http.Error(w, Err_WaitingForPlayers, http.StatusInternalServerError)
+		return
+	}
+	question, err := FindQuestion(w)
+	if err != nil {
+		return
+	}
+	var response StartGameResponse
+	response.Match = match
+	response.Prompt = question
+	json.NewEncoder(w).Encode(response)
+}
 
+func QuestionsCount() (int, error) {
+	cursor, err := mongo.Find(Database, QuestionCollection, bson.M{}, &options.FindOptions{})
+	if err != nil {
+		return 0, err
+	}
+	elements, _ := cursor.Current.Elements()
+	size := len(elements)
+	return size, nil
+}
+
+func FindQuestion(w http.ResponseWriter) (question Question, err error) {
+
+	return
+}
+
+func HandlerAddQuestion(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var requestBody AddQuestionRequest
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		http.Error(w, Err_RequestNotDecoded, http.StatusInternalServerError)
+		return
+	}
+	var question Question
+	question.Id = uuid.New().String()
+	question.Statements = requestBody.Statements
+	requestDto, _ := mongo.Document(question)
+	insertedQuestionId, err := mongo.Write(Database, QuestionCollection, *requestDto)
+	if err != nil {
+		http.Error(w, Err_QuestionNotCreated, http.StatusInternalServerError)
+		return
+	}
+	var answer Answer
+	answer.Id = uuid.New().String()
+	answer.QuestionId = fmt.Sprint(insertedQuestionId.InsertedID)
+	answer.Answer = requestBody.Answer
+	requestDto, _ = mongo.Document(answer)
+	insertedAnswerId, err := mongo.Write(Database, AnswerCollection, *requestDto)
+	if err != nil {
+		http.Error(w, Err_AnswerNotCreated, http.StatusInternalServerError)
+		return
+	}
+	var response AddQuestionResponse
+	response.QuestionId = fmt.Sprint(insertedQuestionId.InsertedID)
+	response.AnswerId = fmt.Sprint(insertedAnswerId.InsertedID)
+	json.NewEncoder(w).Encode(response)
 }
 
 func HandlerNextQuestion(w http.ResponseWriter, r *http.Request) {
 }
 
-func HandlerVerifyAnswer(w http.ResponseWriter, r *http.Request) {
+func HandlerFindAnswer(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var requestBody FindAnswerRequest
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		http.Error(w, Err_RequestNotDecoded, http.StatusInternalServerError)
+		return
+	}
+	dto := mongo.FindOne(Database, AnswerCollection, bson.M{"question_id": requestBody.QuestionId})
+	err = dto.Err()
+	if err != nil {
+		http.Error(w, Err_AnswerNotPresent, http.StatusInternalServerError)
+		return
+	}
+	answer, err := DecodeAnswer(dto)
+	var response FindAnswerResponse
+	response.QuestionId = requestBody.QuestionId
+	response.Answer = answer.Answer
+	json.NewEncoder(w).Encode(response)
+}
+
+// Decodes a mongo db single result into an user object.
+func DecodeAnswer(document *mg.SingleResult) (v Answer, err error) {
+	var answer Answer
+	if err = document.Decode(&answer); err != nil {
+		return answer, err
+	}
+	return answer, err
 }
